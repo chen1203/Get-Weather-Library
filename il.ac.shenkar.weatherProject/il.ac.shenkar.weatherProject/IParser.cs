63465ad84d57060c84e33855a398df5b7d8dd22f
ataliahoren <ataliahoren@gmail.com>
2015-08-24 11:11:26 +0300

    Update IParser.cs
    1-updated the summary
    2-add new method -  List<WeatherData> ParseDocumentFuture(double days); >> this method parse the xml for future forecast between 3-5 days

--- il.ac.shenkar.weatherProject/il.ac.shenkar.weatherProject/IParser.cs
+++ il.ac.shenkar.weatherProject/il.ac.shenkar.weatherProject/IParser.cs
@@ -1,15 +1,17 @@
 ﻿
 namespace il.ac.shenkar.weatherProject
 {
-    /// <summary>
+        /// <summary>
     /// This interface defines the ParseDocument method 
-    /// and its purpose is to get the data from and xml site
+    /// and it's purpose is to get the data from an xml site
+    /// two different parsers - for current weather and for future weather
     /// </summary>
     interface IParser
     {
         /// <summary>
         /// Parses the xml document from the url 
         /// </summary>
-        WeatherData ParseDocument();
+        WeatherData ParseDocumentCurrent();
+        List<WeatherData> ParseDocumentFuture(double days);
     }
 }
