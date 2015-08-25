234de3a38258772506737d9e2ca2ce1d31fdb190
Chen Shamir <chenshamir1203@gmail.com>
2015-08-25 19:03:13 +0300

    location param

--- il.ac.shenkar.weatherProject/il.ac.shenkar.weatherProject/IWeatherDataService.cs
+++ il.ac.shenkar.weatherProject/il.ac.shenkar.weatherProject/IWeatherDataService.cs
@@ -9,7 +9,7 @@
         /// <summary>
         /// Get the current weather details in the requested location
         /// </summary>
-        /// <param Location(city name, country name)> Represents the requested location to find the weather in </param>
+        /// <param name="location"> Represents the requested location to find the weather in </param>
         /// <returns> The weather data object which represents the current weather by location </returns>
         WeatherData GetWeatherData(Location location);
     }
